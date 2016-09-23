f2a5d8ac553002dd4f41c31840188578a6b3fc6f
kosker <Mustafa.Kosker@monitise.com>
2016-09-24 02:04:32 +0300

    Images added to Carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavigationBar from './components/navigation/NavigationBar.jsx';
+import Carousel from './components/carousel/Carousel.jsx';
+
+const carouselImages = [
+  {
+    url: 'https://officesnapshots.com/wp-content/uploads/2016/01/cbre-office-design-9.jpg',
+    title: 'London'
+  },
+  {
+    url: 'http://tech.eu/wp-content/uploads/2015/05/SAM_3618.jpg',
+    title: 'Amsterdam'
+  },
+  {
+    url: 'http://soundcloud-wpengine.netdna-ssl.com/wp-content/uploads/2014/06/SoundCloud-new-Berlin-Office-3.jpg',
+    title: 'Berlin'
+  }
+];
 
 const App = () => (
   <div>
     <div className="carousel-container">
       <NavigationBar title="Deskbookers" />
+      <Carousel images={carouselImages} />
     </div>
   </div>
 );
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const Carousel = () => (
+const Carousel = props => (
   <div className="carousel-wrapper">
     <div className="left-arrow-icon" />
-    <div className="image-content-wrapper" />
+    <div className="image-list-wrapper">
+      {props.images.map(image =>
+        <div className="carousel-image-container">
+          <img src={image.url} className="carousel-image" role="presentation" />
+          <div className="carousel-title">{image.title}</div>
+        </div>
+      )}
+    </div>
     <div className="right-arrow-icon" />
   </div>
 );
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default Carousel;
