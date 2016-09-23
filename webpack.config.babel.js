d17aa3c6fc7bc6745f51e9d782314a382ae0b49a
koskerm <Mustafa.Kosker@monitise.com>
2016-09-23 16:40:14 +0300

    Initial configuration of the project
    - webpack configuration added
    - babel, eslint, jsx and sass loaders added to webpack
    - webpack-dev-server added to project
    - initial page files(index.html, App.jsx, app.scss) added

---
+++ webpack.config.babel.js
@@ -0,0 +1,49 @@
+import path from 'path';
+
+const webpackConfig = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000/',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill', 'App.jsx', 'app.scss'
+  ],
+  output: {
+    publicPath: 'http://localhost:3000/build/',
+    filename: 'deskbookers.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      }
+    ]
+  },
+  resolve: {
+    root: [
+      path.resolve('./src'),
+      path.resolve('./src/components')
+    ]
+  },
+  eslint: {
+    configFile: '.eslintrc.json'
+  },
+  watch: true,
+  devServer: {
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: 3000
+  }
+};
+
+export default webpackConfig;
