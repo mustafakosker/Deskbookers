24adeb2f06870db59db52d7056bf3474f2b45e4e
koskerm <Mustafa.Kosker@monitise.com>
2016-09-23 19:35:54 +0300

    Carousel component added

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Carousel = () => (
+  <div className="carousel-wrapper">
+    <div className="left-arrow-icon" />
+    <div className="image-content-wrapper" />
+    <div className="right-arrow-icon" />
+  </div>
+);
+
+export default Carousel;
