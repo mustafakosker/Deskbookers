05974106a72eb9ae5dfaa9050ccbdad37c966c79
koskerm <Mustafa.Kosker@monitise.com>
2016-09-26 19:04:04 +0300

    ListView styling

--- src/components/resultsView/listView/ListView.jsx
+++ src/components/resultsView/listView/ListView.jsx
@@ -3,16 +3,24 @@ import React, { PropTypes } from 'react';
 const ListView = props => (
   (props.items && props.items.length > 0) ?
     (<div className="list-view-container">
+      <div className="results-title">
+        Search Results
+        <div className="divider" />
+      </div>
       {props.items.map(item => (
         <div className="result-item">
+          <div className="result-image-container">
+            <img src={item.imageUrl} className="item-image" role="presentation" />
+            <div className="hour-price">
+              {`${item.hourPrice}€`}
+              <span> / Hourly</span>
+            </div>
+          </div>
           <div className="name">
             {item.name}
           </div>
           <div className="location-name">
             {item.locationName}
-          </div>
-          <div className="hour-price">
-            {item.hourPrice}
           </div>
           <div className="rating">
             {item.rating}
